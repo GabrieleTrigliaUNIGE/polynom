0e455ead6533a33c83795a6d0744d6a0cd346641
Gabriele Triglia <gabrieletriglia04@gmail.com>
2025-05-08 13:29:05 +0200

    Versione definitiva di Polynom, rinominati i file

--- main.cpp
+++ main.cpp
@@ -1,35 +1,31 @@
-#include <iostream>
-#include "CPolinomio.h"
+#include "CPolynomial.h"
 
 int main() {
-    // Polinomio p(x) = 1 + 2x + 3x^2
-    double coeff1[] = {1.0, 2.0, 3.0};
-    Polinomio p(coeff1, 3);
-    std::cout << "P(x) = ";
-    p.stampa();
-
-    // Valutazione in x = 2
-    double x = 2.0;
-    std::cout << "P(" << x << ") = " << p.valuta(x) << std::endl;
-
-    // Polinomio q(x) = -1 + 0x + 4x^2 + 5x^3
-    double coeff2[] = {-1.0, 0.0, 4.0, 5.0};
-    Polinomio q(coeff2, 4);
-    std::cout << "Q(x) = ";
-    q.stampa();
-
-    // Somma r(x) = p(x) + q(x)
-    Polinomio r = p + q;
-    std::cout << "R(x) = P(x) + Q(x) = ";
-    r.stampa();
-
-    // Derivata di p e q
-    Polinomio dp = p.derivata();
-    Polinomio dq = q.derivata();
-    std::cout << "P'(x) = ";
-    dp.stampa();
-    std::cout << "Q'(x) = ";
-    dq.stampa();
-
-    return 0;
+	
+	double cf1[7]={2.0,1.5,0,0,0,0,-1.0};
+	double cf2[4]={-2.0,1.5,2.5,3.5};
+	
+	
+	Polynomial p0;
+	Polynomial p1(cf1,7);
+	Polynomial p2(cf2,4);
+	Polynomial p3=p1;
+		
+	p0.Dump();
+	p1.Dump();
+	p2.Dump();
+	p3.Dump();
+	
+	p0=p2;
+	p0.Dump();
+	
+	p3 = p0 + p2;
+	p3.Dump();
+	
+	if (p0==p1)
+		cout << "uguali" << endl;
+	
+	p1.Reset(); 
+	p1.Dump();
+	
 }
